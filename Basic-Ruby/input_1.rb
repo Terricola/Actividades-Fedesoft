7622caa478e3364e609568483be2b3526c1638e1
Michael Franco <visoc4@gmail.com>
2018-10-21 13:46:31 -0500

    Primer ejercicio basico en Ruby

---
+++ Basic-Ruby/input_1.rb
@@ -0,0 +1,21 @@
+#El usuario ingresara 2 numeros y se imprimira ṕosteriormente la suma y multiplicacion de
+#estos.
+
+puts "-Terricola Basic Projects-\n"
+puts "Ingrese un numero: "
+num1 = gets.to_i
+puts "====================="
+puts "Ingrese otro numero: "
+num2 = gets.to_i
+
+suma = num1 + num2
+multiplicacion = num1 * num2
+
+puts "La suma es: "
+puts suma
+puts "La multiplicacion es: "
+puts multiplicacion
+
+
+#puts "La suma de los 2 numeros es: "+suma
+#puts "La multiplicacion de los 2 numeros es: "+multiplicacion
